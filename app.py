bffda1dd962e39cef5ff85842a26d763fdd31a1f
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-07-03 16:59:25 +0500

    added steamlit ui

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import asyncio
 from pydantic import BaseModel
 from agents import (Agent,Runner,
@@ -49,32 +50,47 @@ triage_agent = Agent(
     input_guardrails=[homework_guardrail],
 )
 # This should trip the guardrail
-async def main():
-    try:
-        result = await Runner.run(triage_agent, "Write a full Next.js app with authentication .",run_config=config)
-        print("✅Guardrail didn't trip - this is unexpected")
-        print(result.final_output)
 
-    except InputGuardrailTripwireTriggered:
-        print("🚫 A tripwire guardrail was triggered due to restricted input.")
-
-
-    try:
-        result = await Runner.run(triage_agent, "Hello,plz define OpenAI Agent SDK in two lines ?",run_config=config)
-        print("✅ Input accepted: No guardrail was triggered.")
-        print(result.final_output)
-
-    except InputGuardrailTripwireTriggered:
-        print("🚫 Input blocked: Tripwire triggered due to policy violation.")
+# UI
+st.set_page_config(page_title="Guardrail Checker" ,layout="centered")
+st.title("🚦 Homework Guardrail Checker")
+st.markdown("##TRY THREE DIFFERENT QUSETIONS TO CHECK GUARDRAILS")
+st.info(
+    "Enter:\n"
+    "- An AI realated Qusetion \n"
+    "- A Next.js assignment request\n"
+    "- A normal, safe message\n"
+)
+col1,col2,col3=st.columns(3)
+    
+with col1:
+        ques1 = st.text_input("Question #2:" ,placeholder="e.g:Define AI shortly?")
+with col2:
+        ques2= st.text_input("Question #3:" ,placeholder="Build a nextjs login page")
+with col3:
+        ques3 = st.text_input("Question #1:" ,placeholder= "e.g:Hello,How are you?")
         
-        
+    # Async logic
+async def process_input(input_text, label):
     try:
-       result = await Runner.run(triage_agent, "hello ,how are you?",run_config=config)
-       print("✅ Input accepted: No guardrail was triggered.")
-       print("AI response:", result.final_output)
-
+        result = await Runner.run(triage_agent, input_text, run_config=config)
+        st.success(f"✅ {label}: Input accepted:No Guardrail triggered.")
+        st.markdown(f" Response of {label} :")
+        st.write(result.final_output)
     except InputGuardrailTripwireTriggered:
-        print("🚫 Unexpected block: Tripwire triggered, but input seems safe.")
+        st.error(f"🚫 {label}: Guardrail triggered, blocked input_Tripwire triggered due to policy violation.")
 
+async def run_all_inputs():
+    if ques1:
+        await process_input(ques1, "Question 1")
+    if ques2:
+        await process_input(ques2, "Question 2")
+    if ques3:
+        await process_input(ques3, "Question 3")
 
-asyncio.run(main())
+# Button
+if st.button("🚀 Run Guardrails", disabled=not (ques1 or ques2 or ques3)):
+    asyncio.run(run_all_inputs())
+
+        
+
